4f755b6192797d5684a726559403d6f00a6c1122
pathip <43748190+pathip@users.noreply.github.com>
2018-12-19 17:05:21 +0700

    แก้อีกรอบ

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,25 +11,26 @@ namespace ConsoleApp1
         }
         static void Main(string[] args)
         {
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Cal Data of Geometry");
-            }
-
+            
             if (args.Length == 2)
             {
-                float rectangle = AreaRec(float.Parse(args[0]), float.Parse(args[1]));
-                Console.WriteLine($"Rectangle {args[0]}x{args[1]}: " + rectangle);
-                float tri = triangle.AreaTri(float.Parse(args[0]), float.Parse(args[1]));
-                Console.WriteLine($"Triangle 0.5x{args[0]}x{args[1]}: " + tri);
-                float pri = Prism.AreaPri(float.Parse(args[0]), float.Parse(args[1]));
-                Console.WriteLine($"Prism {args[0]}x{args[1]}: " + pri);
-                float pyra = pyramid.AreaPyramid(float.Parse(args[0]), float.Parse(args[1]));
-                Console.WriteLine($"Pyramid {args[0]}x{args[1]}: " + pyra);
-                float dia = Diamond.AreaDiamond(float.Parse(args[0]), float.Parse(args[1]));
-                Console.WriteLine($"Diamond 0.3x{args[0]}x{args[1]}: " + dia);
-                float squ = square.AreaSquare(float.Parse(args[0]));
+                float rectangle, tri, pri, pyra, dia, squ;
+                rectangle = AreaRec(float.Parse(args[0]), float.Parse(args[1]));
+                tri = triangle.AreaTri(float.Parse(args[0]), float.Parse(args[1]));
+                pri = Prism.AreaPri(float.Parse(args[0]), float.Parse(args[1]));
+                pyra = pyramid.AreaPyramid(float.Parse(args[0]), float.Parse(args[1]));
+                dia = Diamond.AreaDiamond(float.Parse(args[0]), float.Parse(args[1]));
+                squ = square.AreaSquare(float.Parse(args[0]));
                 Console.WriteLine($"Square {args[0]}x{args[0]}: " + squ);
+                Console.WriteLine($"Rectangle {args[0]}x{args[1]}: " + rectangle);
+                Console.WriteLine($"Triangle 0.5x{args[0]}x{args[1]}: " + tri);
+                Console.WriteLine($"Prism {args[0]}x{args[1]}: " + pri);
+                Console.WriteLine($"Pyramid {args[0]}x{args[1]}: " + pyra);
+                Console.WriteLine($"Diamond 0.3x{args[0]}x{args[1]}: " + dia);
+            }
+            else
+            {
+                Console.WriteLine("Cal Data of Geometry");
             }
         }
     }
