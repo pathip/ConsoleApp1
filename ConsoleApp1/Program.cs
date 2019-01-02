7cf274d7143bd69ad67257bb865db79f7084f779
pathip <43748190+pathip@users.noreply.github.com>
2019-01-02 16:06:37 +0700

    เสร็จสะที(ว้อย)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,26 +10,36 @@ namespace ConsoleApp1
         {
             float rec , tri, pri;
             IEnumerable<float> pyra, dia, squ;
-                    if (args.Length == 2)
-                    {
-                        rec = Rectangle.AreaRec(float.Parse(args[0]), float.Parse(args[1]));
-                        tri = triangle.AreaTri(float.Parse(args[0]), float.Parse(args[1]));
-                        pri = Prism.AreaPri(float.Parse(args[0]), float.Parse(args[1]));
-                        pyra = pyramid.AreaPyramid(float.Parse(args[0]), float.Parse(args[1]));
-                        dia = Diamond.AreaDiamond(float.Parse(args[0]), float.Parse(args[1]));
-                        squ = square.AreaSquare(float.Parse(args[0]));
-                        Console.WriteLine($"Square {args[0]}x{args[0]}: " + squ);
-                        Console.WriteLine($"Rectangle {args[0]}x{args[1]}: " + rec);
-                        Console.WriteLine($"Triangle 0.5x{args[0]}x{args[1]}: " + tri);
-                        Console.WriteLine($"Prism {args[0]}x{args[1]}: " + pri);
-                        Console.WriteLine($"Pyramid {args[0]}x{args[1]}: " + pyra);
-                        Console.WriteLine($"Diamond 0.3x{args[0]}x{args[1]}: " + dia);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Cal data of Geometry");
-                    }
+            try
+            {
+                rec = Rectangle.AreaRec(float.Parse(args[0]), float.Parse(args[1]));
+                tri = triangle.AreaTri(float.Parse(args[0]), float.Parse(args[1]));
+                pri = Prism.AreaPri(float.Parse(args[0]), float.Parse(args[1]));
+                pyra = pyramid.AreaPyramid(float.Parse(args[0]), float.Parse(args[1]));
+                dia = Diamond.AreaDiamond(float.Parse(args[0]), float.Parse(args[1]));
+                squ = square.AreaSquare(float.Parse(args[0]));
+                foreach (var s in squ)
+                {
+                    Console.WriteLine($"Square {args[0]}x{args[0]}: " + s);
+                }
+                Console.WriteLine($"Rectangle {args[0]}x{args[1]}: " + rec);
+                Console.WriteLine($"Triangle 0.5x{args[0]}x{args[1]}: " + tri);
+                Console.WriteLine($"Prism {args[0]}x{args[1]}: " + pri);
+                foreach (var p in pyra)
+                {
+                    Console.WriteLine($"Pyramid {args[0]}x{args[1]}: " + p);
+                }
+
+                foreach (var d in dia)
+                {
+                    Console.WriteLine($"Diamond 0.3x{args[0]}x{args[1]}: " + d);
+                }
             }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Out of Range Exception");
+            }
+        }
         }
     }
 
